cf265458aa88b3738ce4f581373606a8d45c84dc
sokolovskyi.oleksandr <sokolovskiy.alexandr.dev@gmail.com>
2023-06-07 16:57:27 +0200

    - add layout scroll styles, card overflow styles  - common js

--- assets/scripts/common.js
+++ assets/scripts/common.js
@@ -1,21 +1,61 @@
 export default {
+
+  ROUTER: null,
+
   $el: {
-
+    $navBarEl: null,
+    $backBtnEl: null,
+    $likedPageBtnEl: null,
+    $mainBtnEl: null
   },
 
-  $selectors: {
-
+  selector: {
+    navBarSel: '#navBar',
+    backBtnSel: '#historyBackBtn',
+    likedPageBtnSel: '#likedPageBtn',
+    mainBtnSel: '#mainPageBtn'
   },
 
-  init () {
-    // alert(1);
+  handler: {
+    boundHandleBackClick: null,
+    boundHandleLikedPageClick: null,
+    boundHandleMainPageClick: null,
+  },
+
+  init (ROUTER) {
+    this.ROUTER = ROUTER;
+    this.defineEl()
+    this.defineHandlers()
   },
 
   defineEl () {
-
+    this.$el.$navBarEl = document.querySelector(this.selector.navBarSel);
+    this.$el.$backBtnEl = this.$el.$navBarEl.querySelector(this.selector.backBtnSel);
+    this.$el.$likedPageBtnEl = this.$el.$navBarEl.querySelector(this.selector.likedPageBtnSel);
+    this.$el.$mainBtnEl = this.$el.$navBarEl.querySelector(this.selector.mainBtnSel);
   },
 
   defineHandlers() {
+    this.handler.boundHandleBackClick = this.onBackBtnClick.bind(this);
+    this.$el.$backBtnEl.addEventListener('click', this.handler.boundHandleBackClick);
 
+    this.handler.boundHandleLikedPageClick = this.onLikedPageBtnClick.bind(this);
+    this.$el.$likedPageBtnEl.addEventListener('click', this.handler.boundHandleLikedPageClick);
+
+    this.handler.boundHandleMainPageClick = this.onMainBtnPageClick.bind(this);
+    this.$el.$mainBtnEl.addEventListener('click', this.handler.boundHandleMainPageClick);
   },
+
+  onBackBtnClick() {
+    history.back();
+    this.ROUTER.lastResolved();
+  },
+
+  onLikedPageBtnClick() {
+    this.ROUTER.navigateByName('favourites');
+  },
+
+  onMainBtnPageClick() {
+    this.ROUTER.navigateByName('main');
+  }
 }
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,18 +4,22 @@ import main from "./routes/main";
 import favourites from "./routes/favourites";
 import API from "./utils/api";
 
-const ROUTER = new Navigo('/', { strategy: 'ALL' });
+const ROUTER = new Navigo('/', {strategy: 'ALL'});
 
 const app = async () => {
   API.init();
-  common.init();
+  common.init(ROUTER);
 
   ROUTER.on({
-    '/':  (match) => main.init(match),
-    '/favourites':  (match) => favourites.init(match),
+    '/': {
+      as: 'main',
+      uses: (match) => main.init(match)
+    },
+    '/favourites': {
+      as: 'favourites',
+      uses: (match) => favourites.init(match)
+    },
   }).resolve();
-
-  ROUTER.addBeforeHook('/', (done) => main.destroy(done))
 };
 
 document.addEventListener('DOMContentLoaded', app);
